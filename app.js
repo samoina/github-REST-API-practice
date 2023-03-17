b2035e90ab9eff5f05459fddb511218d7091bfe4
Samoina <samoina.w@gmail.com>
2023-03-17 12:29:16 +0300

    Add JS to search issues

--- app.js
+++ app.js
@@ -7,15 +7,28 @@ const btnRepos = document.querySelector('.main__button--searchRepos');
 const btnIssues = document.querySelector('.main__button--searchIssues');
 const btnCommits = document.querySelector('.main__button--searchCommits');
 const btnCreateIssue = document.querySelector('.main__button--createIssue');
-const main = document.querySelector('.main');
+const article = document.querySelector('.article');
+
+
+async function baseURL(){
+  const url = 'https://api.github.com';
+
+  let response = await fetch(url);
+  let result = await response.json();
+
+  console.log(result);
+}
+
+baseURL();
 
 /*ADD EVENT LISTENERS */
 btnRepos.addEventListener('click', getRepos);
-
+btnIssues.addEventListener('click', getIssues);
 
 /* ASYNC FUNCTIONS */
+/* GET REPOS*/
 async function getRepos(){
-  baseURL();
+  clear();
   
   const url = "https://api.github.com/search/repositories?q=org:freecodecamp"
 
@@ -33,9 +46,11 @@ async function getRepos(){
   let repoPara = document.createElement('p'),
       ol = document.createElement('ol');
 
-  repoPara.textContent = 'I searched for repositories owned by the freeCodeCamp organization';
+  repoPara.innerHTML = 'I searched for repositories owned by the freeCodeCamp organization and here is a list: <br><br>';
 
-  main.appendChild(repoPara);
+  repoPara.style.marginTop = '1rem'
+
+  article.appendChild(repoPara);
 
   reposArr.forEach(repo=>{
     console.log(repo.full_name);
@@ -48,16 +63,52 @@ async function getRepos(){
     ol.appendChild(li);
   })
   
-  main.appendChild(ol);
+  article.appendChild(ol);
 }
 
-async function baseURL(){
-  const url = 'https://api.github.com';
+/* GET ISSUES*/
+async function getIssues(){
+  clear();
+
+  const url = 'https://api.github.com/search/issues?q=freecodecamp/freecodecamp'
 
   let response = await fetch(url);
-  let result = await response.json();
+  let results = await response.json();
 
-  console.log(result);
+  let resultsArr= results.items,
+      para = document.createElement('p'),
+      ol = document.createElement('ol');
+
+  console.log(resultsArr);
+
+ resultsArr.map(result => {
+  let issueStr = result.body;
+  console.log(issueStr);
+
+  let li = document.createElement('li'),
+      anchor = document.createElement('a'),
+      link = result.html_url;
+
+  anchor.setAttribute('href', link);
+  anchor.textContent = `${issueStr}`.substring(0, 120) + '...';
+
+  li.appendChild(anchor);
+  ol.appendChild(li);
+ })
+
+ para.innerHTML = 'I searched for issues within the freeCodeCamp repos and here is a list: <br><br>';
+
+ article.appendChild(para);
+ article.appendChild(ol);
+ 
+}
+
+
+/* CLEAR THE PAGE*/
+function clear(){
+  while(article.firstChild){
+    article.removeChild(article.firstChild);
+  }
 }
 
 
